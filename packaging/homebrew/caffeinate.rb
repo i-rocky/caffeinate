b6367292bd4723114aedd6d1186d0df0cedf6583
Rocky <smrockypk@gmail.com>
2026-02-16 13:52:20 +0600

    Fix macOS release runners and bump to v0.1.1

--- packaging/homebrew/caffeinate.rb
+++ packaging/homebrew/caffeinate.rb
@@ -1,25 +1,25 @@
 class Caffeinate < Formula
   desc "Cross-platform caffeinate command for Linux and Windows"
   homepage "https://github.com/i-rocky/caffeinate"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/i-rocky/caffeinate/releases/download/v0.1.0/caffeinate-darwin-aarch64-v0.1.0.tar.gz"
+      url "https://github.com/i-rocky/caffeinate/releases/download/v0.1.1/caffeinate-darwin-aarch64-v0.1.1.tar.gz"
       sha256 "REPLACE_DARWIN_ARM64_SHA256"
     else
-      url "https://github.com/i-rocky/caffeinate/releases/download/v0.1.0/caffeinate-darwin-x86_64-v0.1.0.tar.gz"
+      url "https://github.com/i-rocky/caffeinate/releases/download/v0.1.1/caffeinate-darwin-x86_64-v0.1.1.tar.gz"
       sha256 "REPLACE_DARWIN_X86_64_SHA256"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/i-rocky/caffeinate/releases/download/v0.1.0/caffeinate-linux-aarch64-v0.1.0.tar.gz"
+      url "https://github.com/i-rocky/caffeinate/releases/download/v0.1.1/caffeinate-linux-aarch64-v0.1.1.tar.gz"
       sha256 "REPLACE_LINUX_ARM64_SHA256"
     else
-      url "https://github.com/i-rocky/caffeinate/releases/download/v0.1.0/caffeinate-linux-x86_64-v0.1.0.tar.gz"
+      url "https://github.com/i-rocky/caffeinate/releases/download/v0.1.1/caffeinate-linux-x86_64-v0.1.1.tar.gz"
       sha256 "REPLACE_LINUX_X86_64_SHA256"
     end
   end
